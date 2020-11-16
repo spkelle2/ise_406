0141158ec4b753dc598fb87e91297f207e16cc9a
Sean Kelley <seanpkelley94@gmail.com>
2020-11-16 18:05:58 -0500

    new guy

--- hw3/runner.sh
+++ hw3/runner.sh
@@ -4,12 +4,12 @@
 #==============================================================================
 # Mosek solver - Simplex
 #==============================================================================
-mosek -d MSK_IPAR_OPTIMIZER MSK_OPTIMIZER_DUAL_SIMPLEX \
--d MSK_DPAR_SIMPLEX_ABS_TOL_PIV 1e-3 \
--d MSK_IPAR_PRESOLVE_USE MSK_PRESOLVE_MODE_OFF \
--d MSK_IPAR_NUM_THREADS 1 \
-BigPrimal.mps \
-| tee output.txt
+#mosek -d MSK_IPAR_OPTIMIZER MSK_OPTIMIZER_DUAL_SIMPLEX \
+#-d MSK_DPAR_SIMPLEX_ABS_TOL_PIV 1e-3 \
+#-d MSK_IPAR_PRESOLVE_USE MSK_PRESOLVE_MODE_OFF \
+#-d MSK_IPAR_NUM_THREADS 1 \
+#BigPrimal.mps \
+#| tee output.txt
 
 #MSK_OPTIMIZER_DUAL_SIMPLEX MSK_OPTIMIZER_PRIMAL_SIMPLEX
 
@@ -17,16 +17,16 @@ BigPrimal.mps \
 #==============================================================================
 # Mosek solver - IPM
 #==============================================================================
-#mosek -d MSK_IPAR_OPTIMIZER MSK_OPTIMIZER_INTPNT \
-#-d MSK_IPAR_INTPNT_BASIS MSK_BI_ALWAYS \
-#-d MSK_DPAR_INTPNT_TOL_DFEAS 1e-3 \
-#-d MSK_DPAR_INTPNT_TOL_PFEAS 1e-3 \
-#-d MSK_DPAR_SIMPLEX_ABS_TOL_PIV 1e-3 \
-#-d MSK_DPAR_INTPNT_TOL_REL_GAP 1e-12 \
-#-d MSK_IPAR_PRESOLVE_USE MSK_PRESOLVE_MODE_OFF \
-#-d MSK_IPAR_NUM_THREADS 1 \
-#BigDual.mps \
-#| tee output.txt
+mosek -d MSK_IPAR_OPTIMIZER MSK_OPTIMIZER_INTPNT \
+-d MSK_IPAR_INTPNT_BASIS MSK_BI_ALWAYS \
+-d MSK_DPAR_INTPNT_TOL_DFEAS 1e-3 \
+-d MSK_DPAR_INTPNT_TOL_PFEAS 1e-3 \
+-d MSK_DPAR_SIMPLEX_ABS_TOL_PIV 1e-3 \
+-d MSK_DPAR_INTPNT_TOL_REL_GAP 1e-12 \
+-d MSK_IPAR_PRESOLVE_USE MSK_PRESOLVE_MODE_OFF \
+-d MSK_IPAR_NUM_THREADS 1 \
+BigPrimal.mps \
+| tee output.txt
 
 # MSK_BI_ALWAYS MSK_BI_NEVER
 
